f2831c0bae665f388059feb4152d4dcb59c7a065
SmirnoffRD <38382018+SmirnoffRD@users.noreply.github.com>
2018-11-29 16:22:12 +0300

    Основной шаблон

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
+from shop.settings import STATICFILES_DIRS
+import json
+import os
+
 
 # Create your views here.
 def main_view(request):
     return render(request, 'mainapp/index.html')
 
 def contact_view(request):
-    return render(request, 'mainapp/contact.html')
+    path = os.path.join(STATICFILES_DIRS[0], 'contacts.json')
+    with open(path) as data:
+        cont_info = json.load(data)
+
+    return render(request, 'mainapp/contact.html', {"continfo": cont_info,})
 
 def products_view(request):
-    return render(request, 'mainapp/products.html', {'data': 'COOL'})
+    return render(request, 'mainapp/products.html')
