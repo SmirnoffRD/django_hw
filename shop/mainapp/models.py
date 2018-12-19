e6dd06c01f05b7a5d800434336c00208b9e167fe
SmirnoffRD <38382018+SmirnoffRD@users.noreply.github.com>
2018-12-19 13:31:00 +0300

    Super user + product  added

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=32, unique=True, verbose_name='Название')
+    descrition = models.TextField(verbose_name='Описание товара')
+    price = models.PositiveIntegerField(verbose_name='Цена')
+# class Product_Type(models.Model):
+#     pass
