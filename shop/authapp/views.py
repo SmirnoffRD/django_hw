473e926f0b2420baa8436d68bf5302279767a1c3
SmirnoffRD <38382018+SmirnoffRD@users.noreply.github.com>
2018-12-23 20:19:13 +0300

    Нестабильная версия с почти полным 4ым дз

--- shop/authapp/views.py
+++ shop/authapp/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from authapp.forms import CustomUser_LoginForm, CustomUser_RegistrationForm
+from django.contrib import auth
 # Create your views here.
+
+def login_view(request):
+    title = 'Вход'
+
+    login_form = CustomUser_LoginForm(data=request.POST or None)
+    if request.method == 'POST' or login_form.is_valid():
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user and user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main'))
+    
+    content = {'title': title, 'login_form': login_form}
+    return render(request, 'authapp/login.html', content)
+
+def registration_view(request):
+    title = 'Регистрация'
+
+    register_form = CustomUser_RegistrationForm()
+
+    if request.method == 'POST':
+        register_form = CustomUser_RegistrationForm(request.POST, request.FILES)
+
+        if register_form.is_valid():
+            register_form.save()
+            return HttpResponseRedirect(reverse('authapp:login'))
+    else:
+        register_form = CustomUser_RegistrationForm()
+    
+    content = {'title': title, 'register_form': register_form}
+    return render(request, 'authapp/register.html', content)
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('mainapp:main'))
--- shop/mainapp/urls.py
+++ shop/mainapp/urls.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
 from django.urls import path
 
 from mainapp.views import contact_view, main_view, products_view
@@ -8,20 +7,15 @@ from mainapp.models import Category
 
 app_name = 'mainapp'
 
-# categories = Category.objects.all()
+categories = Category.objects.all()
 urlpatterns = [
     path('', main_view, name='main'),
     path('contacts/', contact_view, name='contacts'),
     path('products/', products_view, name='products'),
 ]
 
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-# for category in categories:
-#     urlpatterns.append(path('products/{}/'.format(category.id), products_view, name='{}'.format(category.name)))
+if categories:
+    for category in categories:
+        urlpatterns.append(path('products/{}/'.format(category.id), products_view, name='{}'.format(category.name)))
 
 print(urlpatterns)
